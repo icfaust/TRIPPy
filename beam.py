6356d7572461ef97cf038692f1f60b5b873e79ac
faustian <icfaust@gmail.com>
2013-09-24 13:22:40 -0400

    3d plotting routines added

--- beam.py
+++ beam.py
@@ -2,3 +2,5 @@ import geometry
 import surface
 
 # generate necessary beams for proper inversion (including etendue, etc)
+class Beam():
+""" generates an origin with defined etendue and non scalar.  Beams are the
---
+++ plot/plot.py
@@ -0,0 +1,1 @@
+import mayavi,scipy,eqtools
