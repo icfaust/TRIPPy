f0c9cf1893bf47c86d81eee56ac4813d45ca6ec7
icfaust <icfaust@gmail.com>
2013-10-11 11:55:23 -0400

    BPLY update

--- BPLY.py
+++ BPLY.py
@@ -1,4 +1,4 @@
-import AXUV20
+import AXUV
 import surface
 import geometry
 import scipy
@@ -11,7 +11,7 @@ def BPLY(temp, place=(1.87,0,.157277), angle=(0,-.17453-scipy.pi/2,1.62385-scipy
     Vec = [geometry.Vecx((1.,0.,0.)),geometry.Vecx((0.,0.,1.))]
     aperature = surface.Rect((0.,0.,0.),pos,area,Vec=Vec)
     offset = geometry.Origin((-2.5e-3,0.,-7.389e-2),aperature,Vec=Vec)
-    diodes = AXUV20.AXUV22(offset)
+    diodes = AXUV.AXUV22(offset)
     for i in diodes:
         i.redefine(temp)
     diodes.append(aperature)
