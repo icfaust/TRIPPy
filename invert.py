6317548d9f94190ce2a76fe57bb4090dba6394a5
faustian <icfaust@gmail.com>
2013-11-23 16:52:10 -0500

    invert beginning

--- invert.py
+++ invert.py
@@ -1,2 +1,14 @@
 import plasma
 import beam
+import scipy
+
+
+def sens(beams,plasmameth,time,points):
+    """ optimal to give multiple times """
+    time = scipy.atleast_1d(time)
+    # initialize output array of sensitivities
+    output = scipy.zeros((len(time),len(beams),len(points)))
+    
+
+    for i in xrange(len(time)):
+        
