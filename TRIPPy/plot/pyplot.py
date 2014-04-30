3b0c69158bcac82428dba56b81d03496af3a7a15
faustian <icfaust@gmail.com>
2014-04-30 16:25:34 -0400

    added

--- TRIPPy/plot/pyplot.py
+++ TRIPPy/plot/pyplot.py
@@ -3,23 +3,23 @@ import TRIPPy
 import matplotlib.pyplot as plt
 
 
-def plotTokamak(tokamak, axis=True, **kwargs):
+def plotTokamak(tokamak, axis=True, *args, **kwargs):
 
-    plt.plot(tokamak.sagi.s,tokamak.norm.s,**kwargs)
+    plt.plot(tokamak.sagi.s, tokamak.norm.s, *args, **kwargs)
     if axis:
         plt.axis('equal')
 
-def plotLine(line, invessel=True, ds=2.5e-3, **kwargs):
+def plotLine(line, invessel=True, ds=2.5e-3, *args, **kwargs):
     
     try:
         if invessel:
             temp = line(scipy.mgrid[line.norm.s[-2]:line.norm.s[-1]:ds])
         else:
             temp = line(scipy.mgrid[line.norm.s[0]:line.norm.s[-1]:ds])
-        plt.plot(temp.r0(), temp.x2(), **kwargs)
+        plt.plot(temp.r0(), temp.x2(), *args, **kwargs)
     except AttributeError:
         for i in line:
-            plotLine(i, invessel=invessel, **kwargs)
+            plotLine(i, invessel=invessel, *args, **kwargs)
 
 def sinogram(beam, r, z, invessel=True, ds=2.5e-3, *args, **kwargs):
     
