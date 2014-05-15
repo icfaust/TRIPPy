0fe65d0bdb9cd0149c0b86bd4b4a8f386223204b
faustian <icfaust@gmail.com>
2014-05-15 19:29:39 -0400

    changes?

--- TRIPPy/plot/loadct.py
+++ TRIPPy/plot/loadct.py
@@ -1,6 +1,11 @@
 import scipy
 from scipy.io import readsav
 import matplotlib.colors
+import os
+import sys
+import inspect
+cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0]))
 
-def __eval__(num):
-    print(num)
+def loadct(num, **kwargs):
+    output = readsav(cmd_folder+'/idl_colors.sav').output.T
+    return matplotlib.colors.LinearSegmentedColormap.from_list('idl', output[num].T, **kwargs)
