a2a0d238befe26e276510aeb560fdfbbe7f025e7
faustian <icfaust@gmail.com>
2014-05-15 19:47:30 -0400

    finally working colortables

--- TRIPPy/plot/loadct.py
+++ TRIPPy/plot/loadct.py
@@ -6,7 +6,7 @@ import inspect
 cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0]))
 
 def loadct(num, **kwargs):
-    output = scipy.readtxt(cmd_folder+'/idl_colors.txt',
-                           skip_header=256*num,
-                           skip_footer=(38-num)*256)/255.
+    output = scipy.genfromtxt(cmd_folder+'/idl_colors.txt',
+                              skip_header=256*num,
+                              skip_footer=(38-num)*256)/255.
     return matplotlib.colors.LinearSegmentedColormap.from_list('idl', output, **kwargs)
