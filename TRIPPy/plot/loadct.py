1cca34d301b33ab12075e288a8268b958bd1a149
faustian <icfaust@gmail.com>
2014-05-15 20:45:24 -0400

    fully added loadct to work with mayavi, need to add documentation eventually

--- TRIPPy/plot/loadct.py
+++ TRIPPy/plot/loadct.py
@@ -5,8 +5,16 @@ import sys
 import inspect
 cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0]))
 
-def loadct(num, **kwargs):
-    output = scipy.genfromtxt(cmd_folder+'/idl_colors.txt',
-                              skip_header=256*num,
-                              skip_footer=(39-num)*256)/255.
-    return matplotlib.colors.LinearSegmentedColormap.from_list('idl', output, **kwargs)
+def loadct(num, mayavi=False, **kwargs):
+    if not mayavi:
+
+        output = scipy.genfromtxt(cmd_folder+'/idl_colors.txt',
+                                  skip_header=256*num,
+                                  skip_footer=(39-num)*256)/255.
+        return matplotlib.colors.LinearSegmentedColormap.from_list('idl', output, **kwargs)
+    else:
+        output = scipy.ones((256,4),dtype=int)
+        output[:,0:3] = scipy.genfromtxt(cmd_folder+'/idl_colors.txt',
+                                         skip_header=256*num,
+                                         skip_footer=(39-num)*256,dtype=int)
+        return output
