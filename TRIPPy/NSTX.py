f1b6512f274e572cc2843aad56ec10a77dedb585
faustian <icfaust@gmail.com>
2014-05-16 23:01:20 -0400

    changes to geometry were made

--- TRIPPy/NSTX.py
+++ TRIPPy/NSTX.py
@@ -1,15 +1,15 @@
 import geometry,surface,AXUV
 import scipy
 
-def diode1(temp,place=(2.0,0,2e-2), angle=(0,scipy.pi/2,0)):
+def diode1(temp,place=(2.0,0,-4e-1), angle=(0,scipy.pi/2,0)):
     pos = geometry.Origin(place,temp,angle=angle,flag=False)
-    area = [2e-3,.5e-3]
+    area = [.5e-4,.75e-3] #[2e-3,.5e-3] set for NSTX rather than NSTXU
     centervec = geometry.Vecx(scipy.array((.815,0,.5))-place)
     # point toward the nominal center
     Vec = [centervec,geometry.cross(geometry.Vecx((0,1,0)),centervec)]
     aperature = surface.Rect((0.,0.,0.),pos,area,vec=Vec)
     Vec = [geometry.Vecx((0.,1.,0.)),geometry.Vecx((0.,0.,1.))]
-    offset = geometry.Origin((0,0.,-2.25e-2),aperature,vec=Vec)
+    offset = geometry.Origin((0,0.,-2.15e-2),aperature,vec=Vec)
     diodes = AXUV.AXUV20(offset)
     for i in diodes:
         i.redefine(temp)
@@ -17,15 +17,15 @@ def diode1(temp,place=(2.0,0,2e-2), angle=(0,scipy.pi/2,0)):
     diodes[-1].redefine(temp)
     return diodes
 
-def diode2(temp,place=(2.0,0,-2e-2), angle=(0,scipy.pi/2,0)):
+def diode2(temp,place=(2.0,0,4e-1), angle=(0,scipy.pi/2,0)):
     pos = geometry.Origin(place,temp,angle=angle,flag=False)
-    area = [4e-3,3e-3]
+    area = [.5e-4,.75e-3]
     centervec = geometry.Vecx(scipy.array((.815,0,-.5))-place)
     # point toward the nominal center
     Vec = [centervec,geometry.cross(geometry.Vecx((0,1,0)),centervec)]
     aperature = surface.Rect((0.,0.,0.),pos,area,vec=Vec)
     Vec = [geometry.Vecx((0.,1.,0.)),geometry.Vecx((0.,0.,1.))]
-    offset = geometry.Origin((0,0.,-2.25e-2),aperature,vec=Vec)
+    offset = geometry.Origin((0,0.,-2.15e-2),aperature,vec=Vec)
     diodes = AXUV.AXUV20(offset)
     for i in diodes:
         i.redefine(temp)
@@ -35,13 +35,13 @@ def diode2(temp,place=(2.0,0,-2e-2), angle=(0,scipy.pi/2,0)):
 
 def diode3(temp,place=(.8,0,1.8), angle=(0,scipy.pi/2,0)):
     pos = geometry.Origin(place,temp,angle=angle,flag=False)
-    area = [2e-3,.5e-3]
+    area = [.5e-4,2.5e-4]
     centervec = geometry.Vecx(scipy.array((.815-.075,0,0))-place)
     # point toward the nominal center
     Vec = [centervec,geometry.cross(geometry.Vecx((0,1,0)),centervec)]
     aperature = surface.Rect((0.,0.,0.),pos,area,vec=Vec)
     Vec = [geometry.Vecx((0.,1.,0.)),geometry.Vecx((0.,0.,1.))]
-    offset = geometry.Origin((0,0.,-1.25e-2),aperature,vec=Vec)
+    offset = geometry.Origin((0,0.,-1.75e-2),aperature,vec=Vec)
     diodes = AXUV.AXUV20(offset)
     for i in diodes:
         i.redefine(temp)
@@ -139,4 +139,37 @@ def diode6(center,ap=(2.25e-2,0,8e-2),angler=(0,0,0)):
 
     diodes.append(aperature)
     diodes[-1].redefine(center)
+    return diodes
+
+
+def diode7(temp,place=(1.5,0,1.3), angle=(0,scipy.pi/2,0)):
+    pos = geometry.Origin(place,temp,angle=angle,flag=False)
+    area = [2e-3,.5e-3]
+    centervec = geometry.Vecx(scipy.array((-1.,0,0)))
+    # point toward the nominal center
+    Vec = [centervec,geometry.cross(geometry.Vecx((0,1,0)),centervec)]
+    aperature = surface.Rect((0.,0.,0.),pos,area,vec=Vec)
+    Vec = [geometry.Vecx((0.,1.,0.)),geometry.Vecx((0.,0.,1.))]
+    offset = geometry.Origin((0,0.,-4e-2),aperature,vec=Vec)
+    diodes = AXUV.AXUV20(offset)
+    for i in diodes:
+        i.redefine(temp)
+    diodes.append(aperature)
+    diodes[-1].redefine(temp)
+    return diodes
+
+def diode8(temp,place=(1.5,0,-1.3), angle=(0,scipy.pi/2,0)):
+    pos = geometry.Origin(place,temp,angle=angle,flag=False)
+    area = [2e-3,.5e-3]
+    centervec = geometry.Vecx(scipy.array((-1.,0,0)))
+    # point toward the nominal center
+    Vec = [centervec,geometry.cross(geometry.Vecx((0,1,0)),centervec)]
+    aperature = surface.Rect((0.,0.,0.),pos,area,vec=Vec)
+    Vec = [geometry.Vecx((0.,1.,0.)),geometry.Vecx((0.,0.,1.))]
+    offset = geometry.Origin((0,0.,-4e-2),aperature,vec=Vec)
+    diodes = AXUV.AXUV20(offset)
+    for i in diodes:
+        i.redefine(temp)
+    diodes.append(aperature)
+    diodes[-1].redefine(temp)
     return diodes
